dfa5dc3cc80f826b9f5da664f5688da900aa7462
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-04-17 13:27:04 +0200

    Signed-off-by: zpengc-bbc <chirametphong.penglerd@bbcag.ch>

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -85,7 +85,7 @@ public class Player extends MapObject {
 			
 			BufferedImage spritesheet = ImageIO.read(
 				getClass().getResourceAsStream(
-					"/Sprites/Player/playersprites.gif"
+					"/Sprites/Player/Untitled.png"
 				)
 			);
 			
